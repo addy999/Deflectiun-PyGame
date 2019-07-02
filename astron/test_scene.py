9bc1df1142c43fe7e9140d3d52a2566c3afd1714
Addy Bhatia <addy@loftorbital.com>
2019-07-01 20:06:49 -0400

    level has escape velocity condition now

--- astron/test_scene.py
+++ astron/test_scene.py
@@ -1,5 +1,6 @@
 import pygame
 import os, sys
+import math
 sys.path.append('./')
 from assests import *
 from utilities import *
@@ -9,10 +10,12 @@ pygame.font.init()
 
 # Setup screen
 screen_x, screen_y = 500, 500
-screen = pygame.display.set_mode((screen_x, screen_y))
+screen = pygame.display.set_mode((screen_x, screen_y), pygame.FULLSCREEN)
 font = pygame.font.SysFont('Helvetica Neue', 30)
-win_region_x = (screen_x/2-50, screen_x/2+50)
+win_region_x = (0.0, 0.0)
+win_region_y = (0.0, 100.0)
 won = False
+min_speed = 190.0
 
 # background
 stars = pygame.image.load(r'C:\Users\addym\Documents\Gravity Assists\astron\stars_1.jpg')
@@ -29,21 +32,25 @@ clock = pygame.time.Clock()
 color = (0, 128, 255)
 
 # Assets
-sc = Spacecraft('Test', 225, 450, 100, thrust_force = 3000, gas_level = 3500)
+sc = Spacecraft('Test', 225, 450, 100, thrust_force = 3000, gas_level = 1000)
 orbit = Orbit(100, 300, 0, screen_y, CW=False, orbit_period = 100.0, progress = -np.pi/8)
 orbit2 = Orbit(100, 300, screen_x, screen_y/2, CW=True, orbit_period = 80.0, progress = np.pi*0.8)
 planet = Planet('Test', mass = 1e16, orbit = orbit)
 planet2 = Planet('Test2', mass = 2e16, orbit = orbit2)
-planets = [planet, planet2]
+
+orbit3 = Orbit(300, 100, screen_x*0.75, screen_y*0.3, CW=True, orbit_period = 80.0, progress = np.pi/2)
+planet3 = Planet('Test', mass = 2e16, orbit = orbit3)
+
+planets = [planet3]
 
 while not done:
         
-        screen.fill((0, 0, 0))
+        # screen.fill((0, 0, 0))
         # screen.blit(stars, (0,0))
         screen.blit(stars_scaled, rect)
         
         for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                         done = True
                 if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                         sc.brakes = not sc.brakes
@@ -62,7 +69,7 @@ while not done:
                         sc.thrust = False
                
         # Goal post
-        pygame.draw.line(screen, (0.0, 255, 174), (win_region_x[0], 0.0), (win_region_x[1], 0.0), 15)
+        pygame.draw.line(screen, (0.0, 255, 174), (0.0, win_region_y[0]), (0.0, win_region_y[1]), 15)
         
         # Planet
         for planet in planets:
@@ -73,65 +80,79 @@ while not done:
         
         # Update spacecraft positions 
         sc.refresh(planets)
-        pygame.draw.polygon(screen, color, [
-                [sc.x, sc.y],
-                [sc.x-25, sc.y+35],
-                [sc.x+25, sc.y+35],
-                ])
-        if sc.thrust:
-                if sc.thrust_direction == '+y':
-                        pygame.draw.polygon(screen, (255, 100, 0), [
-                                [sc.x, sc.y],
-                                [sc.x-5, sc.y-10],
-                                [sc.x+5, sc.y-10],
-                                ])
-                if sc.thrust_direction == '-y':
-                        pygame.draw.polygon(screen, (255, 100, 0), [
-                                [sc.x, sc.y+35],
-                                [sc.x-5, sc.y+35+10],
-                                [sc.x+5, sc.y+35+10],
-                                ])
-                if sc.thrust_direction == '-x':
-                        pygame.draw.polygon(screen, (255, 100, 0), [
-                                [sc.x-10, sc.y+35/2],
-                                [sc.x-20, sc.y+35/2-5],
-                                [sc.x-20, sc.y+35/2+5],
-                                ])
-                if sc.thrust_direction == '+x':
-                        pygame.draw.polygon(screen, (255, 100, 0), [
-                                [sc.x+10, sc.y+35/2],
-                                [sc.x+20, sc.y+35/2-5],
-                                [sc.x+20, sc.y+35/2+5],
-                                ])
+        vel_matrix = sc.vel.rot_matrix
+        tip = [sc.x, sc.y]
+        a = np.matmul(vel_matrix, [sc.x-25,sc.y+35])
+        b = np.matmul(vel_matrix, [sc.x+25,sc.y+35])
+        
+        # pygame.draw.polygon(screen, color, [
+        #         tip,
+        #         a,
+        #         b,
+        #         ])
+        # print(sc.vel, sc.vel.getTheta())
+        # print(sc.vel.vec - sc.bodyTransform(sc.vel.vec))
+        # if sc.thrust:
+        #         if sc.thrust_direction == '+y':
+        #                 pygame.draw.polygon(screen, (255, 100, 0), [
+        #                         tip,
+        #                         tip+np.matmul([-5,-10], vel_matrix),
+        #                         tip+np.matmul([5,-10], vel_matrix),
+        #                         ])
+        #         if sc.thrust_direction == '-y':
+        #                 pygame.draw.polygon(screen, (255, 100, 0), [
+        #                         tip+np.matmul([0,45], vel_matrix),
+        #                         tip+np.matmul([-5, 55], vel_matrix),
+        #                         tip+np.matmul([5, 55], vel_matrix),
+        #                         ])
+        #         if sc.thrust_direction == '-x':
+        #                 pygame.draw.polygon(screen, (255, 100, 0), [
+        #                         [sc.x, sc.y]+np.matmul([-10,35/2], vel_matrix),
+        #                         [sc.x, sc.y]+np.matmul([-20, 35/2-5], vel_matrix),
+        #                         [sc.x, sc.y]+np.matmul([-20, 35/2+5], vel_matrix),
+        #                         ])
+        #         if sc.thrust_direction == '+x':
+        #                 pygame.draw.polygon(screen, (255, 100, 0), [
+        #                         [sc.x, sc.y]+np.matmul([10,35/2], vel_matrix),
+        #                         [sc.x, sc.y]+np.matmul([20,35/2-5], vel_matrix),
+        #                         [sc.x, sc.y]+np.matmul([20,35/2+5], vel_matrix),
+        #                         ])
         vel = 50 * unit_vector((sc.vel.x, sc.vel.y))
-        pygame.draw.line(screen, (0.0, 255, 174), (sc.x, sc.y), (sc.x + vel[0], sc.y + vel[1]), 5)
-         
+        pygame.draw.line(screen, (0.0, 255, 174), tip, tip+vel, 2)
+        pygame.draw.line(screen, (255,255,255), tip, tip-vel, 15)
                  
         # Update screen
-        # print(sc.vel, sc.x, sc.y)
-        if not won:
-                text_surface = font.render(str(sc.gas_level), True, (255,255,255))
-                screen.blit( text_surface, (screen_x-50, screen_y-20))
-        else:
-                text_surface = font.render('Won!', True, (255,255,255))
-                screen.blit( text_surface, (screen_x-50, screen_y-20))
-                done = True
-        
+        text_surface = font.render(str(sc.gas_level), True, (255,255,255))
+        screen.blit( text_surface, (screen_x-50, screen_y-20))
+        text_surface = font.render(str(sc.vel.mag), True, (255,255,255))
+        screen.blit( text_surface, (screen_x-50, screen_y-60))
         
         pygame.display.update()
         clock.tick(FPS)
         
         # check if won or lost
         # Won
-        if win_region_x[0] <= sc.x <= win_region_x[1] and sc.y < 0.0:
+        if sc.x<0.0 and win_region_y[0] <= sc.y <= win_region_y[1] and sc.vel.mag >= min_speed:
                 won = True
+                done = True
         # Out of bounds
         elif not 0.0 < sc.x < screen_x or not 0.0 < sc.y < screen_y:
                 done = True
 
 if won: 
-        print('Won!')      
+        text_surface = font.render('Won!', True, (255,255,255))
 else:
-        print('Loser')
+        text_surface = font.render('Lost :(', True, (255,255,255))
+         
+done = False
+while not done:
+        for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                        done = True
+                        
+        screen.fill((0, 0, 0))
+        screen.blit( text_surface, (screen_x/2, screen_y/2)) 
+        pygame.display.update()
+        clock.tick(FPS)
         
-pygame.quit()
+# pygame.quit()
